4aaae93d620c3876302b90bc7e2c7fbafee57871
Saif Ahmed <sahmed18@in.ibm.com>
2020-06-19 01:17:02 +0530

    catalog service

---
+++ Movie-catalogs-service/src/main/java/io/javabrains/moviecatalogservice/models/Movie.java
@@ -0,0 +1,32 @@
+package io.javabrains.moviecatalogservice.models;
+
+public class Movie {
+
+    private String MovieId;
+
+    public String getMovieId() {
+        return MovieId;
+    }
+
+    public void setMovieId(String movieId) {
+        MovieId = movieId;
+    }
+
+    public Movie() {
+    }
+
+    public Movie(String movieId, String name) {
+        MovieId = movieId;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
